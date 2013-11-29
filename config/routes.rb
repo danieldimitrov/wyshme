4b5ae37bd5e7c878219c3e4de6a67a751a614309
Yury Yantsevich <featalion@gmail.com>
2013-11-29 20:16:31 +0100

    rails naming convention fix; adds items controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Wyshme::Application.routes.draw do
   get "account/index"
-  get "board/index"
+  resources :boards, only: [:index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root "board#index"
+  root "boards#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
