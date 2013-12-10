40be6fa705d956a77e0e9cef05e8ed27c40618c8
Andy Burkovetsky <jamandyjam@gmail.com>
2013-12-10 23:57:35 +0200

    Wysh via URL view / Wysh lists
    https://trello.com/c/KiplqhOi/32-add-wysh-via-url-view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,67 +36,18 @@ $(function() {
 
   $('#register-modal.modal').modal('attach events', '.register-toggler', 'show');
   $('#login-modal.modal').modal('attach events', '.login-toggler', 'show');
-  $('#new-wysh-modal.modal').modal('attach events', '.new-wysh-list', 'show');
-
-  $('#new-wysh-modal .wysh-category .multinput input').on('input', function () {
-    // check is category already created
-    var val = $(this).val().toLowerCase();
-    var alreadyCreated = false;
-    $('#new-wysh-modal .wysh-category .cat-list .item:not(.new-cat) span').each(function(){
-      if(val==$(this).html().toLowerCase()){
-        alreadyCreated = true;
-        return false;
-      }
-    });
-    if($(this).val()=='' || alreadyCreated){
-      $(this).closest('.wysh-category').find('.cat-list .item.new-cat').addClass('disabled');
-    } else {
-      $(this).closest('.wysh-category').find('.cat-list .item.new-cat').removeClass('disabled');
-    }
-  });
-  $('#new-wysh-modal .wysh-category .multinput').on('click', function (e) {
-    e.stopPropagation();
-    $(this).closest('.wysh-category').addClass('active');
-    var input = $(this).find('input');
-    input.focus();
-  });
-  $('#new-wysh-modal .wysh-category .multinput .labels').on('click', function (e) {
-    e.stopPropagation();
-  });
-  $('#new-wysh-modal .cat-list .item').on('click', function (e) {
-    e.stopPropagation();
-    if($(this).hasClass('disabled')){
-      return false;
-    }
-    if($(this).hasClass('new-cat')){
-      var input = $(this).closest('.wysh-category').find('input');
-      var val = input.val();
-      input.val('');
-      $(this).addClass('disabled');
-    }
-    else {
-      var val = $(this).html();
-    }
-    var label = $('<a class="ui label">'+val+' </a>');
-    $('<i class="icon close"></i>')
-      .on('click', function () {
-        $(this).parent('.label').remove();
-        return false;
-      })
-      .appendTo(label);
-    $(this).closest('.input').find('.multinput .labels').append(label);
-  });
-
+  $('#new-wysh-list-modal.modal').modal('attach events', '.new-wysh-list', 'show');
+  $('#wysh-via-url-modal.modal').modal('attach events', '.wysh-via-url', 'show');
 
   $('#wysh-list-toggler').on('click', function(event) {
     event.preventDefault();
     event.stopPropagation();
-    $('#wish-list-brief').toggle();
+    $('#wysh-list-brief').toggle();
   });
   $(document).click(function() {
-    $('#wish-list-brief').hide();
+    $('#wysh-list-brief').hide();
   });
-  $('#wish-list-brief').on('click', function(event) {
+  $('#wysh-list-brief').on('click', function(event) {
     event.stopPropagation();
   });
 
@@ -111,5 +62,38 @@ $(function() {
   $('.active.item .close.button').on('click', function () {
     $('#selected-item').dimmer('hide');
     $('#selected-item').hide();
+  });
+
+  // forms
+  $('#wysh-via-url-modal .ui.form').form({
+    url: {
+      identifier  : 'url',
+      rules: [
+        {
+          type   : 'url',
+          prompt  : 'Please add valid URL'
+        }
+      ]
+    }
+  });
+  $('#new-wysh-list-modal .ui.form').form({
+    name: {
+      identifier  : 'name',
+      rules: [
+        {
+          type   : 'empty',
+          prompt  : 'Name can\'t be empty'
+        }
+      ]
+    },
+    date: {
+      identifier  : 'date',
+      rules: [
+        {
+          type   : 'empty',
+          prompt  : 'Date can\'t be empty'
+        }
+      ]
+    }
   });
 });
