b79de296ea59eb400fff5f75bfbbea8f53c31cc1
Andy Burkovetsky <jamandyjam@gmail.com>
2013-12-14 04:41:36 +0200

    selected item show fixed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -59,15 +59,8 @@ $(function() {
 
   $('#account-sidebar.sidebar').sidebar({overlay: true}).sidebar('toggle');
 
-  $('#wysh-list .item:not(.active) .image, #wysh-list .item:not(.active) .name').on('click', function(event) {
-    event.preventDefault();
-    $('#selected-item').show();
-    $('#selected-item').dimmer('show');
-  });
-
   $('.active.item .close.button').on('click', function () {
     $('#selected-item').dimmer('hide');
-    $('#selected-item').hide();
   });
 
   // forms
