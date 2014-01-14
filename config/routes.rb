172a47e23d520920b87a210541f5b246f5c978f2
Yaron <ysadka@gmail.com>
2014-01-14 15:04:49 -0800

    route naming fixup

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Wyshme::Application.routes.draw do
   get '/mock/wysh-list/shared',     to: 'mocks#shared_wysh',   as: 'mock_shared_wysh'   # Shared With Me (Wysh List)
   get '/mock/wysh-list/search',     to: 'mocks#search_wysh',   as: 'mock_search_wysh'   # Search (Wysh List)
   get '/mock/wysh-list/suggested',  to: 'mocks#suggested',     as: 'mock_suggested'     # Suggested (Wysh List)
-  get '/mock/events/shared-events', to: 'mocks#shared_events', as: 'mock_shared_events' # Shared Events (Events)
+  get '/mock/events/shared',        to: 'mocks#shared_events', as: 'mock_shared_events' # Shared Events (Events)
   get '/mock/events/search',        to: 'mocks#search_events', as: 'mock_search_events' # Search (Events)
   get '/mock/peeps/see-all',        to: 'mocks#see_all',       as: 'mock_see_all'       # Ses All (Peeps)
 
