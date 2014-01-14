d103fd8b7e489f3a9875ce8fcffe4e2f8c1a11b7
Yaron <ysadka@gmail.com>
2014-01-14 15:25:44 -0800

    pages controller/routes for ‘/wysh’ ‘/events’ and ‘/catalog’

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Wyshme::Application.routes.draw do
 
-  resources :events, only: [:index]
   resources :lists,  only: [:index]
   resources :items,  only: [:index]
 
   get 'templates(/:action)', controller: :templates
 
   get 'account' => 'account#index'
+
+  get 'catalog', to: 'pages#catalog'
+  get 'events',  to: 'pages#events'
+  get 'wysh',    to: 'pages#wysh'
 
   root 'items#index'
 
