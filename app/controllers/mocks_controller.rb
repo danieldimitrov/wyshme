956334a01e833bf61438abfd985bcb41d5ea9ed9
Yaron <ysadka@gmail.com>
2014-01-13 16:41:47 -0800

    routes for mock pages

---
+++ app/controllers/mocks_controller.rb
@@ -0,0 +1,20 @@
+class MocksController < ApplicationController
+
+  def shared_wysh
+  end
+
+  def search_wysh
+  end
+
+  def suggested
+  end
+
+  def shared_events
+  end
+
+  def search_events
+  end
+
+  def see_all
+  end
+end
